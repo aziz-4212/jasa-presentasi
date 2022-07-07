e80923eb4a41ee1e3e43252b4b4f95a731394260
Muhammadakhlakul <akhlakulaziz27@gmail.com>
2022-07-07 15:46:31 +0700

    configurasi role

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -31,19 +31,40 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+        // Fortify::createUsersUsing(CreateNewUser::class);
+        // Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        // Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        // Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $email = (string) $request->email;
+        // RateLimiter::for('login', function (Request $request) {
+        //     $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
-        });
+        //     return Limit::perMinute(5)->by($email.$request->ip());
+        // });
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
+        // RateLimiter::for('two-factor', function (Request $request) {
+        //     return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        // });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+            });
+    
+            Fortify::registerView(function () {
+            return view('auth.register');
+            });
+    
+            Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+            });
+
+            Fortify::verifyEmailView(function () {
+                return view('auth.verify');
+            });
+    
+            Fortify::resetPasswordView(function ($request) {
+                return view('auth.reset-password', ['request' => $request]);
+            });
+    
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['verified']);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
